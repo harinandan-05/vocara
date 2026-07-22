0b3d68ec1a8d661a384bc3d2e6bd179f02c53284
Harinandan <harinandannn@gmail.com>
2026-07-22 20:59:46 +0530

    intitialized the nextjs froontend and a nodejs backend in monorepo

---
+++ apps/frontend/app/dashboard/page.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+
+export default function Dashboard(){
+    const [linkedin,setLinkedin] = useState('');
+    const [github,setGithub] = useState('')
+
+    async function getUrl(){
+
+    }
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
